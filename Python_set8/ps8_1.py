1c71ab495c67743a464ba28c59031d27d582e99d
beggsgrace <beggsgrace@gmail.com>
2025-10-19 12:25:30 -0400

    adding updated ps8

--- Python_set8/ps8_1.py
+++ Python_set8/ps8_1.py
@@ -13,38 +13,40 @@ G_count = 0
 C_count = 0
 count_dict = {}
 # create empty dict 
+
+#to make dictionary of dictionaries that contains information based on string that you will throw away,
+#first make dictionary that will include key of seq header and sequence --> can then later use for loop to recall keys from this dict to iterate over sequences
 with open("test8.fasta", "r") as fastafile1:
-    readable = fastafile1.read()
-    for line in fastafile1:
-        line = line.rstrip()
-        linenumber += 1
+    #readable = fastafile1.read()
+   # for line in fastafile1:
+       # line = line.rstrip()
+      #  linenumber += 1
         #this makes the >seq lines keys in the dict that I will use for counts
-        if '>' in line:
-            line = line.lstrip('>')
-            key1 = re.match(r'\S+', line).group(0)
-            fasta_dict[key1] = []
+       # if '>' in line:
+           # line = line.lstrip('>')
+          #  key1 = re.match(r'\S+', line).group(0)
+            #fasta_dict[key1] = {}
+          #  seq_dict[key1] = {}
+          #  print(fasta_dict)
           #new variable
     #this for loop is to build a dict that will just store id and seq so that I can use it for counting (the counts and seq ID will go in another dictionary)
-    for line in readable.split('\n'): #make readable into lines again so can read line by line
+    for line in fastafile1: #iterate over lines in fastafile
        # print(line) 
-        if '>' in line:
-            ids = re.match(r'\S+', line).group(0)
-            seq_dict[ids] = ''
-        if '>' not in line:
-            seq_dict[curr_id] += line  #add line to second item in list in value of dict
-        else:
-            curr_id=re.match(r'\S+', line).group(0)
-    #this block will count the nts in the seq_dict
-    seq_list = seq_dict.values()
-    for line in readable.split('\n'):
-        key2 = re.match(r'>\S+', readable).group(0)
-        count_dict[key2] = {}
-    for seq in seq_list:
-           A_count = seq.count('A')
-           T_count = seq.count('T')
-           G_count = seq.count('G')
-           C_count = seq.count('C')
-           count_dict[key2][seq] = {'A':A_count, 'T':T_count, 'G':G_count, 'C':C_count}
+       line = line.rstrip() #remove \n at end of line
+       if '>' in line:
+            curr_id = re.match(r'>(\S+)', line).group(1) #this makes curr_id match the first subpattern
+            #seq_dict[ids] = ''
+            fasta_dict[curr_id] = '' #this makes the seq id the keys of fasta_dict, which will be a dict of just id and sequences
+       else:
+            fasta_dict[curr_id] += line  #add line to values corresponding to curr_id keys in dict (which we already defined as empty string)
+
+for curr_id in fasta_dict:  #iterates over keys in fasta_dict
+        seq = fasta_dict[curr_id] #this defines the values in the fasta_dict as a new variable
+        A_count = seq.count('A') #note counting the "A's" in each seq pulled from fasta_dict
+        T_count = seq.count('T')
+        G_count = seq.count('G')
+        C_count = seq.count('C')
+        count_dict[curr_id] = {'A':A_count, 'T':T_count, 'G':G_count, 'C':C_count} #this is separate dict with keys (curr_id) defined above, and then count gets added as dict to values corresponding to curr_id
 
         
 
